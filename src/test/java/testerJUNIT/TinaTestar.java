0e2aca670672b972ce0a17cf22b2edc517a884fd
TinaS-Test <silfvertina@gmail.com>
2026-03-23 13:13:37 +0100

    Lagt in tester i TinasTester, som kollar funktionerna i cmmon-package

--- src/test/java/testerJUNIT/TinaTestar.java
+++ src/test/java/testerJUNIT/TinaTestar.java
@@ -1,23 +1,49 @@
 package testerJUNIT;
+
 import com.example.decathlon.common.CalcTrackAndField;
-import org.junit.jupiter.api.Test; // JUnit 5 (Jupiter)
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.decathlon.common.InputName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class TinaTestar {
 
+    //tester av common-klasserna
+
     @Test
-    void testing(){
+    void testingCalculateTrack(){
         CalcTrackAndField calc = new CalcTrackAndField();
         //A, B och C tas från tabellen, distance och time är deltagarens prestation
         //tex: deltagaren gör 100m sprint på 15 sekunder -> time = 15
         //sedan mixas resultattiden in med lite siffror baserade på statistik
         //och vi får ett resultat, här 185 poäng
-
+        //100M Sprint Decathlon
         double a = 25.4347, b = 18, c = 1.81, time = 15.00;
         int result = calc.calculateTrack(a, b, c, time);
 
         assertEquals(185, result);
         System.out.println("Testet gick igenom för Tina!");
     }
+
+    @Test
+    void testingCalculateField(){
+        //Discus Throw Decathlon
+        CalcTrackAndField calc = new CalcTrackAndField();
+        double a = 12.91, b = 4, c = 1.1, distance = 70;
+        int result = calc.calculateField(a, b, c, distance);
+        assertEquals(1295, result);
+    }
+
+    @Test
+    void testingAddCompetitor(){
+        //Körs förevigt då while-loopen aldrig får en regex-matchning
+        InputName inputName = new InputName();
+        String compName = "Bob Dylan";
+        inputName.addCompetitor();
+
+        assertEquals("Bob Dylan", compName);
+    }
+
+    
 }
