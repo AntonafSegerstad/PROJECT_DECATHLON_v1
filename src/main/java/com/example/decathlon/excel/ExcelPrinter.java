284bdada4ef5c6e68a53a8105e8fa971b1a7eaf9
staff <staffan.aaberg@gmail.com>
2026-03-25 08:31:10 +0100

    Updated ExcelPrinter, ExcelReader and MainGUI

--- src/main/java/com/example/decathlon/excel/ExcelPrinter.java
+++ src/main/java/com/example/decathlon/excel/ExcelPrinter.java
@@ -8,50 +8,50 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class ExcelPrinter {
 
-	private XSSFWorkbook workbook;
-	private String excelName;
+	private final XSSFWorkbook workbook;
+	private final String filePath;
 
-	public ExcelPrinter(String name) throws IOException {
+	public ExcelPrinter(String filePath) throws IOException {
 		workbook = new XSSFWorkbook();
-		excelName = name;
+		this.filePath = filePath;
 	}
 
 	public void add(Object[][] data, String sheetName) {
-
 		XSSFSheet sheet = workbook.createSheet(sheetName);
 
 		int rowCount = 0;
-
-		for (Object[] aBook : data) {
-			Row row = sheet.createRow(rowCount);
-			rowCount++;
+		for (Object[] rowData : data) {
+			Row row = sheet.createRow(rowCount++);
 			int columnCount = 0;
 
-			for (Object field : aBook) {
-				Cell cell = row.createCell(columnCount);
-				columnCount++;
-				
-				if (field instanceof String) {
-					cell.setCellValue((String) field);
-					
-				} else if (field instanceof Integer) {
+			for (Object field : rowData) {
+				Cell cell = row.createCell(columnCount++);
+
+				if (field instanceof Integer) {
 					cell.setCellValue((Integer) field);
-					
 				} else if (field instanceof Double) {
 					cell.setCellValue((Double) field);
-					
+				} else if (field instanceof Long) {
+					cell.setCellValue((Long) field);
+				} else {
+					cell.setCellValue(field == null ? "" : String.valueOf(field));
 				}
+			}
+		}
+
+		if (data.length > 0) {
+			for (int i = 0; i < data[0].length; i++) {
+				sheet.autoSizeColumn(i);
 			}
 		}
 	}
 
 	public void write() throws IOException {
-		FileOutputStream out = new FileOutputStream("C:/Eclipse/resultat_" + excelName + ".xlsx");
-		workbook.write(out);
+		try (FileOutputStream out = new FileOutputStream(filePath)) {
+			workbook.write(out);
+		}
 		workbook.close();
 	}
-
 }
